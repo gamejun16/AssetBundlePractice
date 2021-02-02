64004b1d5d33c41bbbfa459f05ec27c19afd925b
Minjun Jin <gamejun16@daum.net>
2021-02-02 18:30:06 +0900

    1.0.3 Build Resolution 적용. AssetBundle X

---
+++ AssetBundlePractice/AssetBundlePractice/Assets/CanvasManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] private Transform photo;
+    [SerializeField] private Transform album;
+
+
+    private void Start()
+    {
+        photo.gameObject.SetActive(false);
+        album.gameObject.SetActive(false);
+    }
+
+    public void On(int numb)
+    {
+        if(numb == 0) // photo
+        {
+            photo.gameObject.SetActive(!photo.gameObject.activeSelf);
+        }
+
+        if(numb == 1) // album
+        {
+            album.gameObject.SetActive(!album.gameObject.activeSelf);
+        }
+    }
+}
